035bc22648ea76b575038aa70276c769bb6f35a9
harsh6754 <210306105012@paruluniversity.ac.in>
2023-09-25 10:25:24 +0530

    DSA Problems

--- lovebubble/queue/QueueImplementation.cpp
+++ lovebubble/queue/QueueImplementation.cpp
@@ -8,12 +8,24 @@ int main()
     q.push(2);
     q.push(3);
     q.push(4);
+
+    cout<<"Front of q is ->"<<" "<<q.front()<<endl;
     
     cout<<"The Size of Queue->"<<q.size()<<endl;
 
     q.pop();
+    q.pop();
+
+     cout<<"Front of q is ->"<<" "<<q.front()<<endl;
 
      cout<<"The Size of Queue->"<<q.size()<<endl;
+
+    if(q.empty()){
+        cout<<"Queue is empty "<<endl;
+    }
+    else{
+        cout<<"Queue is not empty "<<endl;
+    }
 
     return 0;
 
