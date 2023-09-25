ca62f4b9a8e5a2a3e981d30990a41fbf446f9c04
harsh6754 <210306105012@paruluniversity.ac.in>
2023-09-25 10:18:01 +0530

    DSA Problems

---
+++ lovebubble/queue/QueueImplementation.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    queue<int>q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+    
+    cout<<"The Size of Queue->"<<q.size()<<endl;
+
+    q.pop();
+
+     cout<<"The Size of Queue->"<<q.size()<<endl;
+
+    return 0;
+
+}
