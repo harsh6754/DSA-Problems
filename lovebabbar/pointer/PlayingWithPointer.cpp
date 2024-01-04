e60bebdcc068f4a0914095882a3f24c9363fa100
harsh6754 <210306105012@paruluniversity.ac.in>
2024-01-04 08:49:22 +0530

    Pointer

--- lovebabbar/pointer/PlayingWithPointer.cpp
+++ lovebabbar/pointer/PlayingWithPointer.cpp
@@ -11,4 +11,48 @@ int main()
     int *ptr = &n;
     cout<<*ptr<<endl;
     cout<<ptr<<endl;
+
+
+    double d ;
+    cin>>d;
+    double *ptr2 = &d;
+    cout<<*ptr2<<endl;
+    cout<<ptr<<endl;
+
+    cout<<sizeof(n)<<endl;
+    cout<<sizeof(d)<<endl;
+    cout<<sizeof(ptr)<<endl;
+    cout<<sizeof(ptr2)<<endl;
+
+cout<<endl;
+    int i;
+    cin>>i;
+
+    int *q = &i;
+    cout<<q<<endl;
+    cout<<*q<<endl;
+
+    int *p = 0;
+    p = &i;
+
+    cout<<p<<endl;
+    cout<<*p<<endl;
+
+    cout<<endl;
+
+    int pointer;
+    cin>>pointer;
+    cout<<"a before"<<pointer<<endl;
+    int a = pointer;
+    a++;
+    cout<<"a after"<<pointer<<endl;
+
+    int *pt = &pointer;
+    cout<<"before"<<pointer<<endl;
+    (*pt)++;
+    cout<<"After"<<pointer<<endl;
+
+    return 0;
+
+
 }
