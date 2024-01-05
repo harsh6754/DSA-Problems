2603cfcc0126feb654ea9c29edb4206fc5b4b04d
harsh6754 <210306105012@paruluniversity.ac.in>
2024-01-05 08:14:30 +0530

    DSA PROBLEMS

--- lovebabbar/pointer/Array-In-Pointer/PlayinyWithArrayUsingPointer.cpp
+++ lovebabbar/pointer/Array-In-Pointer/PlayinyWithArrayUsingPointer.cpp
@@ -3,10 +3,18 @@ using namespace std;
 
 int main()
 {
-    int arr[100] = {0};
+    int arr[100] = {23,122,41,67};
     cout<<arr<<endl;
     cout<<arr[0]<<endl;
     cout<<&arr[0]<<endl;
+
+    cout<<*arr<<endl;
+    cout<<*arr +1<<endl;
+    cout<<*(arr+1)<<endl;
+    cout<<(*arr) + 1<<endl;
+    cout<<arr[2]<<endl;
+    cout<<*(arr+2)<<endl;
+    cout<<*(2+arr)<<endl;
 
     cout<<endl;
 }
