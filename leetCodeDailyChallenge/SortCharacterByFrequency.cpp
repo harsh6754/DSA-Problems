10fc5f100e3aa160a6d781bfa5f225c4780d4a96
harsh6754 <210306105012@paruluniversity.ac.in>
2024-02-07 08:56:08 +0530

    Leetcode Daily Challenge Day - 18

---
+++ leetCodeDailyChallenge/SortCharacterByFrequency.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    string frequencySort(string s) {
+        auto cmp = [](const pair<char, int>& a, const pair<char, int>& b) {
+            return a.second < b.second;
+        };
+        
+        priority_queue<pair<char, int>, vector<pair<char, int>>, decltype(cmp)> pq(cmp);
+        
+        unordered_map<char, int> hm;
+        
+        for (char c : s) {
+            hm[c]++;
+        }
+        
+        for (const auto& entry : hm) {
+            pq.push(make_pair(entry.first, entry.second));
+        }
+        
+        string result = "";
+        while (!pq.empty()) {
+            pair<char, int> p = pq.top();
+            pq.pop();
+            result.append(p.second, p.first);
+        }
+        
+        return result;
+    }
+};
+
+int main() {
+    Solution solution;
+    string input;
+    getline(cin, input);
+    string sortedString = solution.frequencySort(input);
+    cout <<  sortedString << endl;
+    return 0;
+}
