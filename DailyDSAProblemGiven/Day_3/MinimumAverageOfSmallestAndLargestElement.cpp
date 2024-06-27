68765922aa529b67b03e7aa53c2ccb79aa2b1214
harsh6754 <agarwalharsh6583@gmail.com>
2024-06-27 11:05:35 +0530

    STQA P

--- DailyDSAProblemGiven/Day_3/MinimumAverageOfSmallestAndLargestElement.cpp
+++ DailyDSAProblemGiven/Day_3/MinimumAverageOfSmallestAndLargestElement.cpp
@@ -1,22 +1,5 @@
 #include<bits/stdc++.h>
 using namespace std;
-class Solution {
-public:
-    double minimumAverage(vector<int>& nums) {
-        vector<double> ans;
-        sort(nums.begin(), nums.end());
-        while(nums.size()) {
-            double f =(double) nums.front();
-            nums.erase(nums.begin());
-
-            double l = (double)nums.back();
-            nums.pop_back();
-            double avg = (l + f) / 2.0;
-            ans.push_back(avg);
-        }
-        return *min_element(ans.begin(), ans.end());
-    }
-};
 int main(){
     int n;
     cin>>n;
@@ -25,10 +8,8 @@ int main(){
     for(int i = 0;i<n;i++){
         cin>>nums[i];
     }
+
     Solution solution;
-    double result = solution.minimumAverage(nums);
-    cout<<result;
-
-
+    
     return 0;
 }
