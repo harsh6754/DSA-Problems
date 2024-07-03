1c47fabbaa9a02f5b6cf49c46dcd501be7be7bb2
harsh6754 <agarwalharsh6583@gmail.com>
2024-07-03 23:10:22 +0530

    Leetcode

---
+++ DailyDSAProblemGiven/Day-6/ValidPalindrome.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+bool isPalindrome(string s) {
+
+    transform(s.begin(),s.end(),s.begin(),::tolower);
+    int i;
+    for(i=0;i<s.size();i++){
+         if(s[i]>=97&&s[i]<=122|| (s[i]>=48&&s[i]<=57)){
+            continue;
+        }
+        else {
+            s.erase(i,1);
+            i--;
+        }
+    }
+    int r=s.size()-1,l=0;
+    while(l<r){
+        if(s[l]!=s[r])
+            return false ;
+        l++;
+        r--;
+    }
+    return true;
+}
+};
+int main(){
+    string s;
+    getline(cin,s);
+
+    Solution solution;
+    bool result = solution.isPalindrome(s);
+    cout<<result ? 1 : 0;
+    return 0;
+}
