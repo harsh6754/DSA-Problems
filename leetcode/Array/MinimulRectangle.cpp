2523c8fae5cfc774d008a579f7c5b2689ea60d09
harsh6754 <agarwalharsh6583@gmail.com>
2024-08-31 13:43:00 +0530

    Leetcode

---
+++ leetcode/Array/MinimulRectangle.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int rows;
+    int cols;
+    cin>>rows>>cols;
+
+    vector<vector<char>>matrix(rows,vector<cha>(cols));
+    for(int i = 0)
+}
---
+++ leetcode/Maths/PermutationSequence.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+ public:
+  string getPermutation(int n, int k) {
+    string ans;
+    vector<int> nums(n);
+    vector<int> fact(n + 1, 1); 
+
+    iota(nums.begin(), nums.end(), 1);
+
+    for (int i = 2; i <= n; ++i)
+      fact[i] = fact[i - 1] * i;
+
+    --k; 
+
+    for (int i = n - 1; i >= 0; --i) {
+      const int j = k / fact[i];
+      k %= fact[i];
+      ans += to_string(nums[j]);
+      nums.erase(nums.begin() + j);
+    }
+    return ans;
+  }
+};
+
+int main(){
+    int n;
+    cin>>n;
+
+    int k;
+    cin>>k;
+
+    Solution solution;
+    string result = solution.getPermutation(n,k);
+    cout<<result;
+
+    return 0;
+}
