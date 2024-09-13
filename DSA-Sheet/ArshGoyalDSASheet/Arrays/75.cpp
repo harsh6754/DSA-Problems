aa9f302474951e1e4f30b880f8edd760ff1ff9dd
harsh6754 <agarwalharsh6583@gmail.com>
2024-09-14 00:22:06 +0530

    Leetcode DSA Sheet

---
+++ DSA-Sheet/ArshGoyalDSASheet/Arrays/75.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low = 0, mid = 0, high = nums.size() - 1;
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
+    }
+};
+int main(){
+    int n;
+    cin>>n;
+    vector<int>nums(n);
+    for(int i = 0;i<n;i++){
+        cin>>nums[i];
+    }
+    Solution solution;
+    solution.sortColors(nums);
+    for(int i = 0;i<n;i++){
+        cout<<nums[i]<<" ";
+    }
+    return 0;
+}
