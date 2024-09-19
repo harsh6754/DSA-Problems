1f53595fee0aaf9bb4e8a47b634ca516d48a958d
harsh6754 <agarwalharsh6583@gmail.com>
2024-09-19 11:07:20 +0530

    Neetcode Question 1523

---
+++ DSA-Sheet/NeetCode150/MathsAndGeometric/CountOddNumberINRange.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+    int countOdds(int low, int high){
+        int ans=0;
+        for(int i = low;i<=high;i++){
+            if(i%2==1){
+              ans+=1;
+            }
+        }
+        return ans;
+}
+};
+int main(){
+    int low;
+    int high;
+    cin>>low>>high;
+
+    Solution solution;
+    int result = solution.countOdds(low,high);
+    cout<<result;
+
+    return 0;
+}
