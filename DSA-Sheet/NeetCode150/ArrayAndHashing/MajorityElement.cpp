1a1f1a0e66ba07f844951e547906396a8b476ac2
harsh6754 <agarwalharsh6583@gmail.com>
2024-09-23 13:43:44 +0530

    Neetcode Question

--- DSA-Sheet/NeetCode150/ArrayAndHashing/MajorityElement.cpp
+++ DSA-Sheet/NeetCode150/ArrayAndHashing/MajorityElement.cpp
@@ -1,5 +1,21 @@
-#include<bits/stdc++.h>
+#innclude<bits/stdc++.h>
 using namespace std;
+class Solution{
+    public:
+    int majorityElement(vector<int>&nums){
+        int size = nums.size();
+        unordered_map<int,int>mp;
+        for(int i = 0;i<size;i++){
+            mp[nums[i]]++;
+        }
+       for(auto& i : mp) {
+            if(i.second > size / 2) {
+                return i.first;
+            }
+        }
+        return -1;
+    }
+};
 int main(){
     int n;
     cin>>n;
@@ -8,5 +24,10 @@ int main(){
     for(int i = 0;i<n;i++){
         cin>>nums[i];
     }
-    
+
+    Solution solution;
+    int result = solution.majorityElement(nums);
+    cout<<result;
+
+    return 0;
 }
