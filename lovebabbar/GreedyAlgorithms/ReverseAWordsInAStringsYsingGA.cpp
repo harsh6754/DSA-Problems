32275a1b74f58676a11ccf4740c7a0472d67ed91
harsh6754 <agarwalharsh6583@gmail.com>
2024-10-06 22:11:24 +0530

    Greedy Algoritms (Reverse A Word in a strin) {Gfg}

---
+++ lovebabbar/GreedyAlgorithms/ReverseAWordsInAStringsYsingGA.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+    string reverseWords(string S){
+        int length = S.length();
+        string ans = " ";
+        string temp = " ";
+
+        for(int i = length - 1;i>=0;i--){
+            if(S[i] == '.'){
+                reverse(temp.begin(),temp.end());
+                ans = ans + temp;
+                temp = " ";
+            }else{
+                temp.push_back(S[i]);
+            }
+        }
+        reverse(temp.begin(),temp.end());
+        ans = ans + temp;  
+        return ans;
+    }
+};
+int main(){
+    string S;
+    getline(cin,S);
+
+    Solution solution;
+    string result = solution.reverseWords(S);
+    cout<<result;
+
+    return 0;
+}
