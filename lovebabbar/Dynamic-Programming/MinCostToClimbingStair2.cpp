7b6950acc1acf3dd7d6bc557ead471775d2f5918
harsh6754 <agarwalharsh6583@gmail.com>
2024-10-15 11:39:32 +0530

    Dynamic Programming (Min Costs To Climbing Stair (Top Down Approach (Recursion + Memoization)))(Leetcode)

---
+++ lovebabbar/Dynamic-Programming/MinCostToClimbingStair2.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+
+    int solve(vector<int>& costs, int n,vector<int>& dp){
+        if(n == 0) return costs[0];
+        if(n == 1) return costs[1];
+
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+
+        dp[n] = costs[n] + (solve(costs,n-1,dp) + solve(costs,n-2,dp));
+        return dp[n];
+    }
+
+    int minCostToClimbingStair(vector<int>& costs){
+        int n = costs.size();
+        vector<int>dp(n+1,-1);
+        int ans = min(solve(costs,n-1,dp),solve(costs,n-2,dp));
+        return ans;
+    }
+};
+
+int main(){
+    int n;
+    cin>>n;
+
+    vector<int>costs(n);
+    for(int i = 0;i<n;i++){
+        cin>>costs[i];
+    }
+
+    Solution solution;
+    int result = solution.minCostToClimbingStair(costs);
+    cout<<result;
+
+    return 0;
+}
