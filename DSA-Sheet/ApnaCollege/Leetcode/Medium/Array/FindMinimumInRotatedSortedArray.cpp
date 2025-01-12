b2ffa640ac726e9a0bf89a6f2934affef18579d2
harsh6754 <agarwalharsh6583@gmail.com>
2025-01-12 12:58:38 +0530

    DSA Sheet

--- DSA-Sheet/ApnaCollege/Leetcode/Medium/Array/FindMinimumInRotatedSortedArray.cpp
+++ DSA-Sheet/ApnaCollege/Leetcode/Medium/Array/FindMinimumInRotatedSortedArray.cpp
@@ -8,11 +8,9 @@ public:
         while (left < right) {
             int mid = left + (right - left) / 2;
 
-            // If mid element is greater than the rightmost, the minimum is in the right half.
             if (nums[mid] > nums[right]) {
                 left = mid + 1;
             } 
-            // Otherwise, the minimum is in the left half (including mid).
             else {
                 right = mid;
             }
